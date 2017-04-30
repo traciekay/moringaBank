a73727eb5e46a3c7f35968f2381d0739f1fe3a12
traciekay <tkaonje@gmail.com>
2017-04-30 13:25:47 +0300

    added business logic

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-$(document).ready(function() {
-  $("#form1").submit(function(event){
-  var a = parseInt($("input#sideA").val());
-  var b = parseInt($("input#sideB").val());
-  var c = parseInt($("input#sideC").val());
-  var text = "";
-    if (a+b <= c || a+c <= b || c+b <=a) {
-      text += "Not a Triangle"
-    }
-    else if ((a===b && b!==c && c!==a) || (a!==b && b!==c && c===a) || (a!==b && b===c && c!==a)) {
-      text += "Isoscless Triangle"
-    }
-    else if (a===b && b===c && c===a) {
-      text += "Equilateral"
-    }
-    else {
-      text += "Scalene Triangle"
-    }
-    document.getElementById("demo").innerHTML = text;
-    event.preventDefault();
+function BankAccount(name,initDeposit){
+  this.custName=name;
+  this.custDeposit=initDeposit;
+}
+
+
+// User interface code
+
+$(document).ready(function(){
+  $("form#customer").submit(function(event){
+
   });
- });
+});
