4ab9f020783401efa8b795bf0318fc7b03aaeba1
traciekay <tkaonje@gmail.com>
2017-04-30 14:00:45 +0300

    added deposit and withdraw prototype

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-function BankAccount(name,initDeposit){
+function BankAccount(name,initDeposit,newDeposit,withdraw){
   this.custName=name;
   this.custDeposit=initDeposit;
+  this.custNewDeposit=newDeposit;
+  this.withdraw=withdraw;
 }
-
+BankAccount.prototype.deposit= function(){
+  return this.custDeposit +
+}
 
 // User interface code
 
 $(document).ready(function(){
   $("form#customer").submit(function(event){
+    event.preventDefault();
+
+    var InputName = $("input#name").val();
+    var InputDeposit = parseInt($("input#initDeposit").val());
+    var InputnewDeposit = parseInt($("input#deposit").val());
+    var InputWithdraw = parseInt($("input#withdraw").val());
+
+    var newCustomer = new BankAccount(InputName, InputDeposit);
+
+    $("span#currentBalance").text(InputDeposit);
 
   });
 });
